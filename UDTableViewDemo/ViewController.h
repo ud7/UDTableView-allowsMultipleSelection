8c5f515a024e73ed0b771acd19e0408274153caa
Yasuhiro Inami <inamiy@gmail.com>
2012-03-13 18:33:46 +0900

    Added UDTableViewDemo & bug note when allowsMultipleSelection=YES & allowsMultipleSelectionDuringEditing=NO.

---
+++ UDTableViewDemo/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  UDTableViewDemo
+//
+//  Created by Yasuhiro Inami on 12/03/13.
+//  Copyright (c) 2012 Yasuhiro Inami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UITableViewController
+
+@property (nonatomic, retain) NSArray* dataStrings;
+
+@end
